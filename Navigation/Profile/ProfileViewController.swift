ae8e930839d253e09c1c45bb382c41af6fd062a9
Minus <sergej.omelchenko.02@mail.ru>
2022-05-12 02:13:33 +0300

    -Все пункты дз выполнены

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -6,13 +6,18 @@
 //
 
 import UIKit
+import StorageService
 
 class ProfileViewController: UIViewController {
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        #if DEBUG
         self.view.backgroundColor = .white
+        #else
+        self.view.backgroundColor = .systemBlue
+        #endif
         configurateTableView()
     }
     
